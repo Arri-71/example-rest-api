6818aeff1095bf4a0c5b189d4d8858709303b6f3
Nicolasgosu <nicolasgosu@unisabana.edu.co>
2022-09-18 19:29:17 -0300

    unit test for addbook

---
+++ src/test/java/co/edu/unisabana/usuario/dao/testBookDAO.java
@@ -0,0 +1,46 @@
+package co.edu.unisabana.usuario.dao;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import co.edu.unisabana.usuario.repository.dao.BookDao;
+import co.edu.unisabana.usuario.service.library.model.Book;
+import co.edu.unisabana.usuario.service.library.model.CategoryBook;
+
+
+public class testBookDAO {
+    @Test
+    public void given_an_existing_book_wehn_add_book_return_true(){
+        BookDao testBookDao = new BookDao();
+        testBookDao.registerBook(new Book("Don Quijote", 1605, "miguel de servantes", false, CategoryBook.SOFT_COVER));
+        assertTrue(testBookDao.addBook("Don Quijote"));
+    }
+    @Test
+    public void given_an_existing_book_wehn_the_number_of_copys_is_equals_to_the_max_number_of_copys_trow_exeption(){
+        BookDao testBookDao = new BookDao();
+        testBookDao.registerBook(new Book("Don Quijote", 1605, "miguel de servantes", false, CategoryBook.SOFT_COVER));
+        for(int i= 1;i<=10;i++){
+            testBookDao.addBook("Don Quijote");
+        }
+        assertThrows(IllegalArgumentException.class, () -> {
+            testBookDao.addBook("Don Quijote");
+        });
+    }
+    @Test
+    public void given_an_inexisting_book_wehn_addbook_trow_exeption(){
+        BookDao testBookDao = new BookDao();
+        testBookDao.registerBook(new Book("Don Quijote", 1605, "miguel de servantes", false, CategoryBook.SOFT_COVER));
+        assertThrows(IllegalArgumentException.class, () -> {
+            testBookDao.addBook("Maria");
+        });
+    }
+}
